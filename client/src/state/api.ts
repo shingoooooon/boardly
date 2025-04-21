0c0dcf7044f0a7c1edd85af5f569d0bccbd77a5d
shingoooooon <skksb75@gmail.com>
2025-04-21 09:17:49 +0900

    implement api frontend

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,54 @@
+import { Project, Task } from "@/types/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBULIC_BASE_URL }),
     reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({})
+    tagTypes: ["Projects", "Tasks"],
+    endpoints: (build) => ({
+        getProjects: build.query<Project[], void>({
+            query: () => "projects",
+            providesTags: ["Projects"]
+        }),
+        createProject: build.mutation<Project, Partial<Project>>({
+            query: (project) => ({
+                url: "projects",
+                method: "POST",
+                body: project
+            }),
+            invalidatesTags: ["Projects"]
+        }),
+        getTasks: build.query<Task[], { projectId: number }>({
+            query: ({ projectId }) => `task?projectId=${projectId}`,
+            providesTags: (result) =>
+                result
+                    ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
+                    : [{ type: "Tasks" as const }]
+        }),
+        createTask: build.mutation<Task, Partial<Task>>({
+            query: (task) => ({
+                url: "tasks",
+                method: "POST",
+                body: task
+            }),
+            invalidatesTags: ["Tasks"]
+        }),
+        updateTaskStatus: build.mutation<Task, { taskId: number; status: string }>({
+            query: ({ taskId, status }) => ({
+                url: `tasks/${taskId}/status`,
+                method: "PATCH",
+                body: { status }
+            }),
+            invalidatesTags: (result, error, { taskId }) => [
+                { type: "Tasks", id: taskId },
+            ]
+        }),
+    })
 })
 
-export const { } = api;
+export const {
+    useGetProjectsQuery,
+    useCreateProjectMutation,
+    useGetTasksQuery,
+    useCreateTaskMutation
+} = api;
---
+++ client/src/types/enums.ts
@@ -0,0 +1,14 @@
+export enum Status {
+    ToDo = "To DO",
+    WorkInProgress = "Work In Progress",
+    InReview = "In Review",
+    Completed = "Completed"
+}
+
+export enum Priority {
+    Urgent = "Urgent",
+    High = "High",
+    Medium = "Medium",
+    Low = "Low",
+    Backlog = "Backlog"
+}
